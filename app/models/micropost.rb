648bf2b965844b7df9c781217dc1bd5459b8e2a2
Nextafari <chinedue16@gmail.com>
2019-09-04 21:17:05 +0000

    Finished toy app

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ApplicationRecord
+  validates :content, length: { maximum 140 }
+                    presence: true
+  belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  root 'application#hello'
+  resources :microposts
+  resources :users
+  root 'users#index'
 end
