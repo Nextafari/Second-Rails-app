ae3d5818f1a0b8650f5ec4f583cf9495c07201c7
Nextafari <chinedue16@gmail.com>
2019-09-11 02:24:47 +0000

    Added sign up form and styled it as well, also created a create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
   # Shows the user a new form to fill up when they make a get request to our sign up page
   def new
+    @user = User.new
   end
+  # This action shows the page of a user, using the find action and params to retrieve info from the database
+  def show
+    @user = User.find(params[:id])
+  end
+ # This action is invoked when a post request is made (when the form is submitted) 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "successful saved"
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
+
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  resources :users
 
  root 'static_pages#home'
 
