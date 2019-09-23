e15574385498136cd8bec866bddfafed7e772b52
Nextafari <chinedue16@gmail.com>
2019-09-23 23:46:19 +0000

    created a forget action in the sessions controller and a helper for it too, also added the remember me feature to the application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       flash.now[:success] = "Signed in successfully"
       redirect_to user
     else
@@ -18,7 +20,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
   
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
-  # Returns the current logged in user (if any)
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # Returns the user corresponding to the remember token cookie
   def current_user
+    if (user_id = session[:user_id])
     @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      raise 
+      user = User.find_by(id: user_id)
+      if user && user.autheticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   # This returns true if the user is logged in, and a false if the user is not
@@ -15,8 +31,17 @@ module SessionsHelper
     !current_user.nil? 
   end
   
+  # Forgets a persistent user
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end  
+  
+  
   # logs out the current user
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
