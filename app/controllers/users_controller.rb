d92d8a96af0c8a7a5090914b64ad00f97e09530d
Nextafari <chinedue16@gmail.com>
2019-09-26 06:54:48 +0000

    created form partial for the user edit and sign up pages, also updated the redirect action for user after edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def update
+    @user = User.find(params[:id])
+    # handles a successful update
+    if @user.update_attributes(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+  
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
   
   delete 'logout', to: 'sessions#destroy'
   
+
 end
