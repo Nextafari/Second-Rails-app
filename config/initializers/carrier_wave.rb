c476566020f3716135468a60f7532c27c80266e3
Nextafari <chinedue16@gmail.com>
2019-10-28 11:00:16 +0000

    Manually added the region to my code

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,16 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp') # adding these...
+    config.cache_dir = 'carrierwave' # ...two lines
+    
     config.fog_credentials = {
-    # Configuration for Amazon S3
-    :provider => 'AWS',
-    :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      # Configuration for Amazon S3
+      :provider => 'AWS',
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],                     # required
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],                    # required
+      :region                 => 'us-east-2'
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
 end
+
