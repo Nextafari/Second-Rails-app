7272df68f473f1931b60c77dc76b9fa1d90008d1
Nextafari <chinedue16@gmail.com>
2019-10-28 11:41:42 +0000

    Modifeied config file

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,13 @@ require_relative 'boot'
 
 require 'rails/all'
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> Stashed changes
+
+>>>>>>> Stashed changes
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
